391d655fb9c6e540716f06d0cfbbef535fbfe01e
김정근 <snorlax@ridi.com>
2018-03-20 19:03:58 +0900

    Add timer logic

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import TokenRefresher from './TokenRefresher';
 
 ((window) => {
   const tokenRefresher = new TokenRefresher();
-  window.runRefresher = () => {
-    tokenRefresher.run();
+  window.startRefresher = () => {
+    tokenRefresher.start();
   };
 
   window.stopRefresher = () => {
