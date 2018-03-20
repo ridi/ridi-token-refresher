302c38c1ea42c91a1cbbe086ff6ab056cedce405
김정근 <snorlax@ridi.com>
2018-03-20 22:04:55 +0900

    Add TokenRefresher Code

--- src/utils/interval.js
+++ src/utils/interval.js
@@ -1,4 +1,4 @@
 
-const DEFAULT_INTERVAL_MARGIN = 300;
+const DEFAULT_INTERVAL_MARGIN = 60 * 10; // 10분 마진
 
 export const calcInterval = (expiresIn, margin = DEFAULT_INTERVAL_MARGIN) => expiresIn - margin;
---
+++ src/utils/request.js
@@ -0,0 +1,21 @@
+import fetch from 'whatwg-fetch';
+
+
+const checkStatus = (response) => {
+  if (response.status >= 200 && response.status < 300) {
+    return response;
+  }
+
+  const error = new Error(response.statusText);
+  error.response = response;
+  throw error;
+};
+
+
+const parseJSON = response => response.json();
+
+
+export const getExpiresIn = async () => {
+  const data = await fetch('https://account.ridibooks.com/ridi/token/').then(checkStatus).then(parseJSON);
+  return data.expires_in;
+};
