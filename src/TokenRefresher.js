2ebd1ee5963a3fbf805088fb16adad35faa0105a
김정근 <snorlax@ridi.com>
2018-03-21 19:11:15 +0900

    Add retry logic

--- src/TokenRefresher.js
+++ src/TokenRefresher.js
@@ -1,12 +1,16 @@
 
 import { calcInterval, getExpiresIn } from './utils';
 
+const MAX_RETRY_COUNT = 3;
+const RETRY_INTERVAL = 5000;
+
 
 export default class TokenRefresher {
   constructor() {
     this.timeoutId = null;
 
     this.loop = this.loop.bind(this);
+    this.retryCount = 0;
   }
 
   start() {
@@ -29,10 +33,15 @@ export default class TokenRefresher {
     try {
       expiresIn = await getExpiresIn();
     } catch (e) {
+      if (this.retryCount < MAX_RETRY_COUNT) {
+        this.retryCount += 1;
+        this.timeoutId = setTimeout(this.loop, RETRY_INTERVAL);
+      }
       return;
     }
 
     const interval = calcInterval(expiresIn);
+    this.retryCount = 0;
     this.timeoutId = setTimeout(this.loop, interval);
   }
 
