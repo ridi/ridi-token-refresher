d70a34601ce95720429a6fedb4ca7758f5e0c5e2
김정근 <snorlax@ridi.com>
2018-03-21 15:32:56 +0900

    Implement logic using async/await

--- src/utils/interval.js
+++ src/utils/interval.js
@@ -1,4 +1,10 @@
 
 const DEFAULT_INTERVAL_MARGIN = 60 * 10; // 10분 마진
+const MICROSECONDS = 1000;
 
-export const calcInterval = (expiresIn, margin = DEFAULT_INTERVAL_MARGIN) => expiresIn - margin;
+export const calcInterval = (expiresIn, margin = DEFAULT_INTERVAL_MARGIN) => {
+  const miscExpiresIn = expiresIn * MICROSECONDS;
+  const miscMargin = margin * MICROSECONDS;
+
+  return miscExpiresIn - miscMargin;
+};
