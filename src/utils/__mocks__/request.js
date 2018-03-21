735712aeade1eb6eff88f95056c3ecae69f8623f
김정근 <snorlax@ridi.com>
2018-03-21 18:36:37 +0900

    Update mock requests

--- src/utils/__mocks__/request.js
+++ src/utils/__mocks__/request.js
@@ -1,5 +1,5 @@
 
-export const getAccountDomain = () => 'ridibooks.com';
+const getAccountDomain = () => 'ridibooks.com';
 
 export const getExpiresIn = async () => {
   const response = await fetch(`https://account.${getAccountDomain()}/ridi/token/`);
